275d767b53c53bae4c9ac20880866ee372e44858
Austin ONeil <aoneil@austinoneil.com>
2023-04-29 22:23:47 -0400

    wrap functions in try except to fix crap code

--- cnc_button_board.py
+++ cnc_button_board.py
@@ -21,61 +21,70 @@ class HomeAssistant:
         response = requests.request("POST", url, headers=self.headers, data=payload)
 
     def turnon_light(self, light, percentage):
-        url = f"{self.HA_BASE_URL}/services/light/turn_on"
-        self.payload = json.dumps({
-        "entity_id": f"light.{light}",
-        "brightness_pct": f"{percentage}",
-        })
-        self.post(self.payload, url)
-    
+        try:
+			url = f"{self.HA_BASE_URL}/services/light/turn_on"
+			self.payload = json.dumps({"entity_id": f"light.{light}","brightness_pct": f"{percentage}"})
+			self.post(self.payload, url)
+		except:
+			print("turnon_light action failed")
+	
     def turnoff_light(self, light):
-        url = f"{self.HA_BASE_URL}/services/light/turn_off"
-        self.payload = json.dumps({
-        "entity_id": f"light.{light}",
-        })
-        self.post(self.payload, url)
-
+        try:
+			url = f"{self.HA_BASE_URL}/services/light/turn_off"
+        	self.payload = json.dumps({"entity_id": f"light.{light}"})
+        	self.post(self.payload, url)
+		except:
+			print("turnoff_light action failed")
+	
     def toggle_light(self, light):
-        url = f"{self.HA_BASE_URL}/services/light/toggle"
-        self.payload = json.dumps({
-        "entity_id": f"light.{light}",
-        })
-        self.post(self.payload, url)
+		try:
+			url = f"{self.HA_BASE_URL}/services/light/toggle"
+			self.payload = json.dumps({
+			"entity_id": f"light.{light}",
+			})
+			self.post(self.payload, url)
+		except:
+			print("toggle_light action failed")
 
     def toggle_switch(self, switch):
-        url = f"{self.HA_BASE_URL}/services/switch/toggle"
-        self.payload = json.dumps({
-        "entity_id": f"switch.{switch}",
-        })
-        self.post(self.payload, url)
+		try:
+			url = f"{self.HA_BASE_URL}/services/switch/toggle"
+			self.payload = json.dumps({"entity_id": f"switch.{switch}"})
+			self.post(self.payload, url)
+		except:
+			print("toggle_switch action failed")
 
     def turn_off_switch(self, switch):
-        url = f"{self.HA_BASE_URL}/services/switch/turn_off"
-        self.payload = json.dumps({
-        "entity_id": f"switch.{switch}",
-        })
-        self.post(self.payload, url)
+		try:
+			url = f"{self.HA_BASE_URL}/services/switch/turn_off"
+			self.payload = json.dumps({"entity_id": f"switch.{switch}"})
+			self.post(self.payload, url)
+		except:
+			print("turn_off_switch action failed")
 
     def turn_on_switch(self, switch):
-        url = f"{self.HA_BASE_URL}/services/switch/turn_on"
-        self.payload = json.dumps({
-        "entity_id": f"switch.{switch}",
-        })
-        self.post(self.payload, url)
+		try:
+			url = f"{self.HA_BASE_URL}/services/switch/turn_on"
+			self.payload = json.dumps({"entity_id": f"switch.{switch}"})
+			self.post(self.payload, url)
+		except:
+			print("turn_on_switch action failed")
 
     def run_automation(self, automation):
-        url = f"{self.HA_BASE_URL}/services/automation/trigger"
-        self.payload = json.dumps({
-        "entity_id": f"automation.{automation}",
-        })
-        self.post(self.payload, url)
-    
+		try:
+			url = f"{self.HA_BASE_URL}/services/automation/trigger"
+			self.payload = json.dumps({"entity_id": f"automation.{automation}"})
+			self.post(self.payload, url)
+		except:
+			print("run_automation action failed")
+
     def run_script(self, script):
-        url = f"{self.HA_BASE_URL}/services/script/turn_on"
-        self.payload = json.dumps({
-        "entity_id": f"script.{script}",
-        })
-        self.post(self.payload, url)
+		try:
+			url = f"{self.HA_BASE_URL}/services/script/turn_on"
+			self.payload = json.dumps({"entity_id": f"script.{script}"})
+			self.post(self.payload, url)
+		except:
+			print("run_script action failed")
 
 # Setup classes
 ha = HomeAssistant()
@@ -97,9 +106,12 @@ ZERO_XYZ_NO_PROBE = ["G92 X0 Y0 Z0"]
 
 # Function to post a message to slack, configured by SLACK_DEFAULT_CHANNEL in env 
 def post_message(message=None, channel=config["SLACK_DEFAULT_CHANNEL"], blocks=None, files=None):
-	message = "MPCNC: " + message
-	slackapi.chat_postMessage(channel=channel, text=message, blocks=blocks, file=files)
-	
+	try:
+		message = "MPCNC: " + message
+		slackapi.chat_postMessage(channel=channel, text=message, blocks=blocks, file=files)
+	except:
+		print("Failed to post message to slack")
+
 # Setup button release & noise Message
 def release(held_for,button):
 	print(f"{button} held for {held_for} seconds.")  
@@ -133,71 +145,83 @@ def multibutton_hld():
 # Trigger 1 - Toggle CNC
 # Trigger 2 - Home XY
 def cncbutton_rls():
-	global held_for
-	release(held_for,"CNC Button")
-	if (held_for >= trigger1time) and (held_for < trigger2time):
-		print('Toggling CNC E-STOP')
-		ha.toggle_switch("cnc_estop")
-	elif (held_for >= trigger2time):
-		print("Home XY")
-		octoapi.gcode(HOME_XY)
-		post_message("Home XY initated")
-	elif (held_for < trigger1time):
-		noise_message("CNC Button")
-	held_for = 0.0
+	try:
+		global held_for
+		release(held_for,"CNC Button")
+		if (held_for >= trigger1time) and (held_for < trigger2time):
+			print('Toggling CNC E-STOP')
+			ha.toggle_switch("cnc_estop")
+		elif (held_for >= trigger2time):
+			print("Home XY")
+			octoapi.gcode(HOME_XY)
+			post_message("Home XY initated")
+		elif (held_for < trigger1time):
+			noise_message("CNC Button")
+		held_for = 0.0
+	except:
+		print("CNC button action failed!")
 
 # Trigger 1 - Toggle Vacuum
 # Trigger 2 - XYZ GT WPH
 # Trigger 3 - Disable Z
 def vacuumbutton_rls():
-	global held_for
-	release(held_for,"Vacuum Button")
-	if (held_for >= trigger1time) and (held_for < trigger2time):
-		print('Toggling Vacuum')
-		ha.toggle_switch("garage_vacuum")
-	elif (held_for > trigger2time) and (held_for < trigger3time):
-		print("XYZ_GT_WPH")
-		post_message("XYZ go to WPH initated")
-		octoapi.gcode(XYZ_GT_WPH)
-	elif (held_for >= trigger3time):
-		print("DISABLE_Z")
-		octoapi.gcode(DISABLE_Z)
-		post_message("Z Axis Disabled")
-	else:
-		noise_message("Vacuum Button")
-	held_for = 0.0
+	try:
+		global held_for
+		release(held_for,"Vacuum Button")
+		if (held_for >= trigger1time) and (held_for < trigger2time):
+			print('Toggling Vacuum')
+			ha.toggle_switch("garage_vacuum")
+		elif (held_for > trigger2time) and (held_for < trigger3time):
+			print("XYZ_GT_WPH")
+			post_message("XYZ go to WPH initated")
+			octoapi.gcode(XYZ_GT_WPH)
+		elif (held_for >= trigger3time):
+			print("DISABLE_Z")
+			octoapi.gcode(DISABLE_Z)
+			post_message("Z Axis Disabled")
+		else:
+			noise_message("Vacuum Button")
+		held_for = 0.0
+	except:
+		print("Vacuum button action failed!")
 
 # Trigger 1 - Toggle CNC Lights
 # Trigger 2 - Zero XYZ w/ Probe		
 def lightbutton_rls():
-	global held_for
-	release(held_for,"Lights Button")
-	if (held_for >= trigger1time) and (held_for < trigger2time):
-		print('Toggling Lights')
-		ha.toggle_switch("cnc_lights")
-	elif (held_for >= trigger2time):
-		print("ZERO_XYZ_PROBE")
-		octoapi.gcode(ZERO_XYZ_PROBE)
-		post_message("Zero XYZ With Probe initated")
-	elif (held_for < trigger1time):
-		noise_message("Lights Button")
-	held_for = 0.0
+	try:
+		global held_for
+		release(held_for,"Lights Button")
+		if (held_for >= trigger1time) and (held_for < trigger2time):
+			print('Toggling Lights')
+			ha.toggle_switch("cnc_lights")
+		elif (held_for >= trigger2time):
+			print("ZERO_XYZ_PROBE")
+			octoapi.gcode(ZERO_XYZ_PROBE)
+			post_message("Zero XYZ With Probe initated")
+		elif (held_for < trigger1time):
+			noise_message("Lights Button")
+		held_for = 0.0
+	except:
+		print("Light button action failed!")
 
 # Trigger 1 - Toggle Garage Fan
 # Trigger 2 - Zero XYZ w/o Probe     
 def multibutton_rls():
-	global held_for
-	release(held_for,"Multi Button")
-	if (held_for >= trigger1time) and (held_for < trigger2time):
-		print('Toggling Fan')
-		ha.toggle_switch("garage_fan")
-	elif (held_for >= trigger2time):
-		print("ZERO_XYZ_NO_PROBE")
-		octoapi.gcode(ZERO_XYZ_NO_PROBE)
-		post_message("Zero XYZ With No Probe initated")
-	elif (held_for < trigger1time):
-		noise_message("Mutli Button")
-	held_for = 0.0
+	try:
+		global held_for
+		release(held_for,"Multi Button")
+		if (held_for >= trigger1time) and (held_for < trigger2time):
+			print('Toggling Fan')
+			ha.toggle_switch("garage_fan")
+		elif (held_for >= trigger2time):
+			print("ZERO_XYZ_NO_PROBE")
+			octoapi.gcode(ZERO_XYZ_NO_PROBE)
+			post_message("Zero XYZ With No Probe initated")
+		elif (held_for < trigger1time):
+			noise_message("Mutli Button")
+		held_for = 0.0
+	except:
+		print("Multi button action failed!")
 
 cncbutton=Button(21, hold_time=trigger1time, hold_repeat=True, pull_up = False)
 vacuumbutton=Button(20, hold_time=trigger1time, hold_repeat=True, pull_up = False)
